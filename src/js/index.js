3ba92cd3f94ea06bd1d4c064528d57623d4a8b79
Kaji Okami <adrian.peso96@gmail.com>
2018-12-14 17:18:02 +0100

    added recording state to future undo/redo (pre undo/redo feature)

--- src/js/index.js
+++ src/js/index.js
@@ -1,48 +1,35 @@
 import * as DOMElements from "./data/DOMElements";
 
 
-console.log(DOMElements);
-let canvas = DOMElements.canvas;
-let cx = canvas.getContext("2d");
+//GLOBAL VARIABLES
+const canvas = DOMElements.canvas;
+const colors = require("./data/colors.json").colors;
+const weights = [2,4,8,10,12,14,16,20];
+
+//CANVAS VARIABLES ?? POSSIBLE OBJECT
 let canvasParent = DOMElements.sectionCanvas;
-let toolbox = DOMElements.toolbox;
-let colorPicker = DOMElements.toolboxColorPicker;
-let weightPicker = DOMElements.toolboxWeightPicker;
 
-let colors = require("./data/colors.json").colors; // <div class="toolbox__color-picker--color" style="background-color:%COLOR%"></div>
-let weights = [2,4,8,10,12,14,16,20]; // <span>%SIZE%px</span><div class="toolbox__weight-picker--line" style="border-bottom: %SIZE%px solid white"></div>
+//TOOLBOX VARIABLES
+let cx = canvas.getContext("2d");
+
+let move = [];
+let history = [];
+
+let overAllHistory = [];
+let pointer = 0; // will be ever pointing on overallHistory
 
 
-console.log(weights);
 //GENERATING PAGE ELEMENTS
-
 colors.forEach(color => { 
-    colorPicker.insertAdjacentHTML("beforeend", `<div data-tool="color" class="toolbox__color-picker--color" style="background-color:${color.hex}"></div>`);
+    DOMElements.toolboxColorPicker.insertAdjacentHTML("beforeend", `<div data-tool="color" class="toolbox__color-picker--color" style="background-color:${color.hex}"></div>`);
 });
 
 weights.forEach(weight => {
-    weightPicker.insertAdjacentHTML("beforeend", `<button data-tool="weight" data-weight="${weight}"><span>${weight}px</span><div class="toolbox__weight-picker--line" style="border-bottom: ${weight}px solid white"></div></button>`);
-});
-
-//TOOLBOX MANUVER
-toolbox.addEventListener('click', event => {
-    if (event.target.getAttribute('data-tool') === 'color') {
-        
-        changeColor(event.target.style.backgroundColor);
-        
-
-    } else if (event.target.getAttribute('data-tool') === 'weight') {
-
-       changeWeight(event.target.getAttribute('data-weight'));
-
-    }
+    DOMElements.toolboxWeightPicker.insertAdjacentHTML("beforeend", `<button data-tool="weight" data-weight="${weight}"><span>${weight}px</span><div class="toolbox__weight-picker--line" style="border-bottom: ${weight*2}px solid"></div></button>`);
 });
 
 //CANVAS SIZING
-canvas.width = canvasParent.offsetWidth - 200;
-canvas.height = canvasParent.offsetHeight - 200;
-
-window.addEventListener('resize', () => {
+let resizeCanvas = () => {
     if(canvasParent.offsetWidth > 600 && canvasParent.offsetHeight > 500){
         canvas.width = canvasParent.offsetWidth - 200;
         canvas.height = canvasParent.offsetHeight - 200;
@@ -50,59 +37,128 @@ window.addEventListener('resize', () => {
         canvas.width = canvasParent.offsetWidth;
         canvas.height = canvasParent.offsetHeight;
     }
+}
+//Initial resizing ( TODO: maybe using onload addeventlistener to start app??)
+resizeCanvas();
+window.addEventListener('resize', () => {
+    resizeCanvas();
+    redrawCanvas(history);
 });
 
 
+//TOOLS
 //CANVAS DRAWING
 let brushWeight = 10;
-let canIdraw = false;
+let canIDraw = false;
+let canIRecord = false;
 
-
-let draw = (ev) => {
-    if(canIdraw){
-        let x = ev.offsetX; // with clientX does some error.
+let drawCanvas = (ev) => {
+    if(canIDraw){
+        let x = ev.offsetX; // with clientX does some error because canvas does not ocupy 100vh/vw.
         let y = ev.offsetY;
 
-        cx.lineWidth = brushWeight * 2;
-
-        cx.lineTo(x, y);
-        cx.stroke();
-        cx.beginPath();
-        cx.arc(x, y, brushWeight, 0, Math.PI*2);
-        cx.fill();
-
-        cx.beginPath();
-        cx.moveTo(x, y);
+        draw(cx.strokeStyle, brushWeight, x, y);
     }
 };
 
-let beginDraw = (ev) => {
-    canIdraw = true;
-    draw(ev);
+let redrawCanvas = history => {
+    history.forEach(element =>{
+        let move = element.move;
+
+        let color = move[0];
+        let weight = move[2];
+
+        move[3].forEach( point => {
+           draw(color,weight, point[0], point[1]);
+        });
+        cx.beginPath();
+    });
+    
+};
+
+let draw = (color, weight, x, y) => {
+
+    cx.fillStyle = color;
+    cx.strokeStyle = color;
+    cx.lineWidth = weight * 2;
+
+    cx.lineTo(x, y);
+    cx.stroke();
+    cx.beginPath();
+    cx.arc(x, y, weight, 0, Math.PI*2);
+    cx.fill();
+
+    cx.beginPath();
+    cx.moveTo(x, y);
+};
+
+let beginDraw = ev => {
+    canIDraw = true;
+    drawCanvas(ev);
 };
 
 let endDraw = () => {
-    canIdraw = false;
+    canIDraw = false;
     cx.beginPath();
 };
 
-canvas.addEventListener('mousedown', beginDraw);
-canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mouseup', endDraw);
+let startRecord = ev => {
+    canIRecord = true;
+    record(ev);
+};
+let record = ev => {
+    if(canIRecord) {
+        let x = ev.offsetX;
+        let y = ev.offsetY;
 
-//TOOLS
+        move.push([x,y]);
+    }
+};
+let endRecord = () => {
+    canIRecord = false;
+
+    history.push({'move': [cx.fillStyle, cx.strokeStyle, brushWeight, move]});
+    move = [];
+}
+
+canvas.addEventListener('mousedown', ev => {
+    beginDraw(ev);
+    startRecord(ev);
+});
+canvas.addEventListener('mousemove', ev => {
+    drawCanvas(ev);
+    record(ev);
+});
+canvas.addEventListener('mouseup', () => {
+    endDraw();
+    endRecord();
+});
+
+
 /**
  * 
  * @param string color  // wich we will pass (DOM element).style.backgroundColor
  *  param accepts HEX and RGBA colors
  */
 let changeColor =  (color) => { 
-    console.log(color);
     cx.fillStyle = color;
     cx.strokeStyle = color;
-    console.log(cx);
 };
 
-let changeWeight = (weight) => {
+let changeWeight = weight => {
     if(weight > 0 && weight < 100) brushWeight = weight;
 };
+
+
+//TOOLBOX option picked
+DOMElements.toolbox.addEventListener('click', event => {
+    if (event.target.getAttribute('data-tool') === 'color') {
+
+        changeColor(event.target.style.backgroundColor);
+        
+    } else if (event.target.getAttribute('data-tool') === 'weight') {
+
+       changeWeight(event.target.getAttribute('data-weight'));
+
+    }
+});
